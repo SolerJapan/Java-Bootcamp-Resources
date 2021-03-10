611cfd86f357abdf1b93f4e8da34d393dcfba9c9
rslim087a <42172016+rslim087a@users.noreply.github.com>
2021-03-10 02:42:08 -0500

    Update FizzBuzz.java

--- Module 1 - Introduction to Java/5. Loops/Workbook 5.7 Solution/FizzBuzz.java
+++ Module 1 - Introduction to Java/5. Loops/Workbook 5.7 Solution/FizzBuzz.java
@@ -4,11 +4,11 @@ public class FizzBuzz {
         for (int i = 0; i < 19; i++) {
             
             if (i % 3 == 0 && i % 5 == 0) {
-                System.out.println("FizzBuzz");
+                System.out.println(i + " FizzBuzz");
             } else if (i % 3 == 0) {
-                System.out.println("Fizz");
+                System.out.println(i + " Fizz");
             } else if (i % 5 == 0) {
-                System.out.println("Buzz");
+                System.out.println(i + " Buzz");
             }
         }
     }
